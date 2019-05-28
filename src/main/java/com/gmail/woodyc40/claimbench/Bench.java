d37628da04c9fba38b875eb2bef01f02744c3354
AgentTroll <woodyc40@gmail.com>
2019-05-28 14:52:48 -0700

    Run benchmark

---
+++ src/main/java/com/gmail/woodyc40/claimbench/Bench.java
@@ -0,0 +1,99 @@
+package com.gmail.woodyc40.claimbench;
+
+import com.mojang.authlib.GameProfile;
+import net.minecraft.server.v1_13_R2.DedicatedServer;
+import net.minecraft.server.v1_13_R2.EntityPlayer;
+import net.minecraft.server.v1_13_R2.PlayerInteractManager;
+import org.bukkit.Bukkit;
+import org.bukkit.craftbukkit.v1_13_R2.CraftServer;
+import org.bukkit.craftbukkit.v1_13_R2.CraftWorld;
+import org.bukkit.craftbukkit.v1_13_R2.entity.CraftPlayer;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+import java.util.concurrent.ThreadLocalRandom;
+
+@State(Scope.Benchmark)
+public class Bench {
+    private final int size = 1000;
+
+    private final UUID[] smallObjects = new UUID[size];
+    private final CraftPlayer[] largeObjects = new CraftPlayer[size];
+
+    private final Set<UUID> addUuid = new HashSet<>();
+    private final Set<UUID> containsUuid = new HashSet<>();
+
+    private final Set<CraftPlayer> addPlayer = new HashSet<>();
+    private final Set<CraftPlayer> containsPlayer = new HashSet<>();
+
+    @Setup
+    public void setup() {
+        for (int i = 0; i < size; i++) {
+            UUID uuid = UUID.randomUUID();
+            smallObjects[i] = uuid;
+
+            CraftWorld spawnWorld = ((CraftWorld) Bukkit.getWorld("world"));
+            CraftServer server = (CraftServer) Bukkit.getServer();
+            DedicatedServer ds = server.getHandle().getServer();
+            GameProfile gp = new GameProfile(uuid, "");
+            EntityPlayer ep = new EntityPlayer(ds, spawnWorld.getHandle(), gp, new PlayerInteractManager(spawnWorld.getHandle()));
+
+            CraftPlayer player = new CraftPlayer(server, ep);
+            largeObjects[i] = player;
+
+            this.containsUuid.add(uuid);
+            this.containsPlayer.add(player);
+            this.containsPlayer.add(player);
+            this.addPlayer.add(player);
+        }
+
+        // Clear entries, retain elementData array size
+        this.addUuid.clear();
+        this.addPlayer.clear();
+    }
+
+    @Benchmark
+    public UUID baselineSmall() {
+        int idx = ThreadLocalRandom.current().nextInt(this.smallObjects.length);
+        return this.smallObjects[idx];
+    }
+
+    @Benchmark
+    public boolean containsSmall() {
+        int idx = ThreadLocalRandom.current().nextInt(this.smallObjects.length);
+        UUID uuid = this.smallObjects[idx];
+        return this.containsUuid.contains(uuid);
+    }
+
+    @Benchmark
+    public boolean addSmall() {
+        int idx = ThreadLocalRandom.current().nextInt(this.smallObjects.length);
+        UUID uuid = this.smallObjects[idx];
+        return this.addUuid.add(uuid);
+    }
+
+    @Benchmark
+    public CraftPlayer baselineLarge() {
+        int idx = ThreadLocalRandom.current().nextInt(this.largeObjects.length);
+        return this.largeObjects[idx];
+    }
+
+    @Benchmark
+    public boolean containsLarge() {
+        int idx = ThreadLocalRandom.current().nextInt(this.largeObjects.length);
+        CraftPlayer player = this.largeObjects[idx];
+        return this.containsPlayer.contains(player);
+    }
+
+    @Benchmark
+    public boolean addLarge() {
+        int idx = ThreadLocalRandom.current().nextInt(this.largeObjects.length);
+        CraftPlayer player = this.largeObjects[idx];
+        return this.addPlayer.add(player);
+    }
+}
--- src/main/java/com/gmail/woodyc40/claimbench/ClaimBench.java
+++ src/main/java/com/gmail/woodyc40/claimbench/ClaimBench.java
@@ -1,61 +1,34 @@
 package com.gmail.woodyc40.claimbench;
 
-import com.mojang.authlib.GameProfile;
-import net.minecraft.server.v1_13_R2.DedicatedServer;
-import net.minecraft.server.v1_13_R2.EntityPlayer;
-import net.minecraft.server.v1_13_R2.PlayerInteractManager;
-import org.bukkit.Bukkit;
-import org.bukkit.World;
-import org.bukkit.craftbukkit.v1_13_R2.CraftServer;
-import org.bukkit.craftbukkit.v1_13_R2.CraftWorld;
-import org.bukkit.craftbukkit.v1_13_R2.entity.CraftPlayer;
-import org.bukkit.craftbukkit.v1_13_R2.entity.CraftWolf;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.runner.Runner;
 import org.openjdk.jmh.runner.RunnerException;
-import sun.jvm.hotspot.oops.ObjectHeap;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
 
-import java.io.IOException;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
-import java.util.UUID;
+import java.io.File;
+import java.net.URL;
+import java.net.URLClassLoader;
 
 public class ClaimBench extends JavaPlugin {
     @Override
     public void onEnable() {
+        // Stole this from: https://stackoverflow.com/questions/35574688/how-to-run-a-jmh-benchmark-in-maven-using-execjava-instead-of-execexec
+        URLClassLoader classLoader = (URLClassLoader) ClaimBench.class.getClassLoader();
+        StringBuilder classpath = new StringBuilder();
+        for (URL url : classLoader.getURLs())
+            classpath.append(url.getPath()).append(File.pathSeparator);
+        System.setProperty("java.class.path", classpath.toString());
+
+        Options options = new OptionsBuilder()
+                .include(Bench.class.getSimpleName())
+                .forks(0)
+                .build();
+
         try {
-            org.openjdk.jmh.Main.main(new String[0]);
+            new Runner(options).run();
         } catch (RunnerException e) {
             e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
         }
     }
-
-    private final int size = 1000;
-
-    private final Object[] smallObjects = new Object[size];
-    private final Object[] largeObjects = new Object[size];
-
-    private final Set<Object> add = new HashSet<Object>();
-    private final Set<Object> contains = new HashSet<Object>();
-
-    @Setup
-    public void setup() {
-        for (int i = 0; i < size; i++) {
-            UUID uuid = UUID.randomUUID();
-            smallObjects[i] = uuid;
-
-            CraftWorld spawnWorld = ((CraftWorld) Bukkit.getWorld("world"));
-            CraftServer server = (CraftServer) Bukkit.getServer();
-            DedicatedServer ds = server.getHandle().getServer();
-            GameProfile gp = new GameProfile(uuid, "");
-            EntityPlayer ep = new EntityPlayer(ds, spawnWorld.getHandle(), gp, new PlayerInteractManager(spawnWorld.getHandle()))
-
-            largeObjects[i] = new CraftPlayer(server, ep);
-        }
-    }
-
-
 }
